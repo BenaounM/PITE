ff4d34dc2c4d1e8a2b831ea6a1c6177e8643e1ec
bilsoidk <bilsidkcomp@gmail.com>
2021-12-18 02:51:46 +0100

    LoginJs Cleaned

--- src/Login.js
+++ src/Login.js
@@ -3,151 +3,58 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
 var apiBaseUrl = "http://localhost:4000/api/";
-import axios from 'axios';
+
 
 class Login extends Component {
   constructor(props){
     super(props);
     var localloginComponent=[];
     localloginComponent.push(
-      <MuiThemeProvider key={"theme"}>
-        <div>
-         <TextField
-           hintText="Enter your College Rollno"
-           floatingLabelText="Student Id"
-           onChange={(event,newValue) => this.setState({username:newValue})}
-           />
-         <br/>
-           <TextField
-             type="password"
-             hintText="Enter your Password"
-             floatingLabelText="Password"
-             onChange = {(event,newValue) => this.setState({password:newValue})}
-             />
-           <br/>
-           <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-       </div>
-       </MuiThemeProvider>
+      
     )
     this.state={
       username:'',
       password:'',
-      menuValue:1,
-      loginComponent:localloginComponent,
-      loginRole:'student'
+     
     }
-  }
-  componentWillMount(){
-  // console.log("willmount prop values",this.props);
-  if(this.props.role != undefined){
-    if(this.props.role == 'student'){
-      console.log("in student componentWillMount");
-      var localloginComponent=[];
-      localloginComponent.push(
-        <MuiThemeProvider>
-          <div>
-           <TextField
-             hintText="Enter your College Rollno"
-             floatingLabelText="Student Id"
-             onChange = {(event,newValue) => this.setState({username:newValue})}
-             />
-           <br/>
-             <TextField
-               type="password"
-               hintText="Enter your Password"
-               floatingLabelText="Password"
-               onChange = {(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>
-             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-         </div>
-         </MuiThemeProvider>
-      )
-      this.setState({menuValue:1,loginComponent:localloginComponent,loginRole:'student'})
-    }
-    else if(this.props.role == 'teacher'){
-      console.log("in teacher componentWillMount");
-      var localloginComponent=[];
-      localloginComponent.push(
-        <MuiThemeProvider>
-          <div>
-           <TextField
-             hintText="Enter your College Rollno"
-             floatingLabelText="Teacher Id"
-             onChange={(event,newValue) => this.setState({username:newValue})}
-             />
-           <br/>
-             <TextField
-               type="password"
-               hintText="Enter your Password"
-               floatingLabelText="Password"
-               onChange={(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>
-             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-         </div>
-         </MuiThemeProvider>
-      )
-      this.setState({menuValue:2,loginComponent:localloginComponent,loginRole:'teacher'})
-    }
-  }
   }
   handleClick(event){
    
   }
-  handleMenuChange(value){
-    console.log("menuvalue",value);
-    var loginRole;
-    if(value==1){
-      var localloginComponent=[];
-      loginRole='student';
-      localloginComponent.push(
-        <MuiThemeProvider>
-          <div>
-           <TextField
-             hintText="Enter your College Rollno"
-             floatingLabelText="Student Id"
-             onChange = {(event,newValue) => this.setState({username:newValue})}
-             />
-           <br/>
-             <TextField
-               type="password"
-               hintText="Enter your Password"
-               floatingLabelText="Password"
-               onChange = {(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>
-             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-         </div>
-         </MuiThemeProvider>
-      )
-    }
-   
-    this.setState({menuValue:value,
-                   loginComponent:localloginComponent,
-                   loginRole:loginRole})
-  }
+  
   render() {
     return (
       <div>
         <MuiThemeProvider>
         <AppBar
-             title="Login"
+             title="Enet'Com"
            />
         </MuiThemeProvider>
         <MuiThemeProvider>
         <div>
-        <p>Login as:</p>
-        <DropDownMenu value={this.state.menuValue} onChange={(event,index,value)=>this.handleMenuChange(value)}>
-          <MenuItem value={1} primaryText="Student" />
-          <MenuItem value={2} primaryText="Teacher" />
-        </DropDownMenu>
+        <p>Login</p>
+       
         </div>
         </MuiThemeProvider>
-        {this.state.loginComponent}
+        <MuiThemeProvider key={"theme"}>
+        <div>
+         <TextField
+           hintText="Taper Votre Email"
+           floatingLabelText="Email"
+           onChange={(event,newValue) => this.setState({username:newValue})}
+           />
+         <br/>
+           <TextField
+             type="password"
+             hintText="Taper Votre Mot De Passe"
+             floatingLabelText="Mot De Passe"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+           <br/>
+           <RaisedButton label="Connexion" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+       </div>
+       </MuiThemeProvider>
       </div>
     );
   }
